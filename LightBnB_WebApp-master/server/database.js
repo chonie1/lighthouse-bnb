b61fc968d49b602123e08c1ea52d105b773c5330
Connie Ho <connie3@ualberta.ca>
2021-03-03 01:33:45 +0000

    update get all prop function with options

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -108,15 +108,83 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  query = {
-    text: `
-    SELECT *
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+    SELECT properties.*, avg(property_reviews.rating) as average_rating
     FROM properties
-    LIMIT $1`,
-    values: [limit]
-  };
+    JOIN property_reviews ON properties.id = property_id
+    `;
+  
+  // Add options if specified
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
 
-  return pool.query(query)
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+      
+    if (queryParams.length > 1) {
+      queryString += `AND owner_id LIKE $${queryParams.length} `;
+    } else {
+      queryString += `WHERE owner_id LIKE $${queryParams.length} `;
+    }
+  }
+
+  // if (options.minimum_price_per_night & options.maximum_price_per_night) {
+  //   queryParams.push(`${options.minimum_price_per_night}`);
+  //   queryParams.push(`${options.maximum_price_per_night}`);
+      
+  //   if (queryParams.length > 2) {
+  //     queryString += `AND cost_per_night >= $${queryParams.length - 1} `;
+  //     queryString += `AND cost_per_night <= $${queryParams.length} `;
+  //   } else {
+  //     queryString += `WHERE cost_per_night >= $${queryParams.length - 1} `;
+  //     queryString += `AND cost_per_night <= $${queryParams.length} `;
+  //   }
+  // }
+    
+  if (options.minimum_price_per_night) {
+    queryParams.push(Number(options.minimum_price_per_night) * 100);
+      
+    if (queryParams.length > 1) {
+      queryString += `AND cost_per_night >= $${queryParams.length} `;
+    } else {
+      queryString += `WHERE cost_per_night >= $${queryParams.length} `;
+    }
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(Number(options.maximum_price_per_night) * 100);
+      
+    if (queryParams.length > 1) {
+      queryString += `AND cost_per_night <= $${queryParams.length} `;
+    } else {
+      queryString += `WHERE cost_per_night <= $${queryParams.length} `;
+    }
+  }
+
+  queryString += `GROUP BY properties.id `;
+
+  if (options.minimum_rating) {
+    queryParams.push(Number(options.minimum_rating));
+    queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
+  }
+  
+  // 4
+  queryParams.push(limit);
+  queryString += `
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+    `;
+  
+  // 5
+  // console.log(queryString, queryParams);
+  
+  // 6
+  return pool.query(queryString, queryParams)
     .then(res => res.rows)
     .catch(err => console.error('query error', err.stack));
 };
